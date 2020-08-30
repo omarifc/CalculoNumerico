450b4d5750a0870637b3c3adcd04660b8021863a
omarifc <57732107+omarifc@users.noreply.github.com>
2020-08-30 14:03:29 -0300

    Add files via upload

---
+++ Práctica1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Práctica1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Esto funciona de maravilla");
+            Console.ReadLine();
+        }
+    }
+}
