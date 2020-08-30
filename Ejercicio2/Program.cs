142731a1fd98662f212e14a3b5c3a6bed899c3de
omarifc <57732107+omarifc@users.noreply.github.com>
2020-08-30 14:16:21 -0300

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region declaración de variables
+            float num1, num2, suma, resta, division, multiplicacion;
+            #endregion
+
+            // Ingreso de datos
+            Console.WriteLine("Ingrese el primer número");
+            num1 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese el segundo número");
+            num2 = float.Parse(Console.ReadLine()); 
+            
+            // Operaciones
+            suma = num1 + num2;
+            Console.WriteLine("La suma de ambos números es: " + suma);
+
+            resta = num1 - num2;
+            Console.WriteLine("La resta de ambos números es: " + resta);
+
+            division = num1 / num2;
+            Console.WriteLine("La división de ambos números es: " + division);
+
+            multiplicacion = num1 * num2;
+            Console.WriteLine("La multiplicación de ambos números es: " + multiplicacion);
+
+            Console.ReadKey();
+        }
+    }
+}
